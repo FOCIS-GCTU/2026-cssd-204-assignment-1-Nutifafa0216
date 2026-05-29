5f8ab66207376ed87c0bfbc632326c1ebfcd4cde
Nutifafa0216 <2425403129@live.gctu.edu.gh>
2026-05-29 11:53:14 +0000

    Add files via upload

---
+++ initilas.py
@@ -0,0 +1,36 @@
+# File: name_initials Assignment2(NF) 2425403129.py
+# Description: Print out my initials.
+# Assignment Number: 2
+
+# Name: Nutifafa Fiadjoe
+# STUDENT ID: 2425403129
+# Email: 2425403129@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+
+# On my honor, NUTIFAFA FIADJOE, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # This program prints my initials in large letters.
+    print(" ")
+
+    print("...NF")
+
+    print(" ")
+
+    print("...NNN.......NN........FFFFFFFFFFFF.....")
+    print("...NNNN......NN........FF...............")
+    print("...NN.NN.....NN........FF...............")
+    print("...NN..NN....NN........FFFFFFFFFFFF.....")
+    print("...NN...NN...NN........FFFFFFFFFFFF.....")
+    print("...NN....NN..NN........FF...............")
+    print("...NN....NN..NN........FF...............")
+    print("...NN.....NN.NN........FF...............")
+    print("...NN......NNNN...**...FF.............**")
+    print("...NN.......NNN...**...FF.............**")
+
+    print(" ")
+
+
+main()
